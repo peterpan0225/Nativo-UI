84f6e1e70d0218d7b857e4813958c265ace8f6cd
peterpan0225 <wasyl.0225@gmail.com>
2022-04-07 11:52:29 -0600

    Adding color gradient bar at top page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import getIcons from "./icons";
 //components
 import Navbar from "./components/Navbar.component";
 import Footer from "./components/Footer.component";
+import ColorBar from "./components/ColorBar.component";
 //views
 import Landing from "./views/Landing.view";
 import Gallery from "./views/gallery";
@@ -62,6 +63,7 @@ class App extends Component {
     return (
       <>
         <Router>
+          <ColorBar/>
           <Navbar theme={this.state.theme} />
           <Switch>
             <Route exact path="/" component={Landing} />
