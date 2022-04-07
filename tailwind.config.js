6c6b8393abb737a1ea6f030f5212e660b38e3647
peterpan0225 <wasyl.0225@gmail.com>
2022-04-07 11:50:59 -0600

    Config Tailwind colors and adding assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    boxShadow: {
+      's': '4px 1px 17px rgba(253, 252, 252, 0.05)',
+    },
+    colors: {
+      'white': '#fff',
+      'darkgray': '#1d1d1b',
+      'yellow':'#f8de44',
+      'yellow2': '#eb8a06',
+      'brown': '#9e0904',
+      'brown2': '#d15904',
+      'yellow3':'#f6bc25',
+      'pink': '#fd644f',
+      'melon': '#f28e26',
+      'hotpink': '#ff0f47',
+      'lightpink': '#ffab96',
+      'blue': '#3c65aa',
+      'purple': '#c6379c'
+    },
+    extend: {
+    }
   },
   plugins: [],
 }
