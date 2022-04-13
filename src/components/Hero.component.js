56a35195b5179f7f18f2434ef8b8fec2b27a3700
peterpan0225 <wasyl.0225@gmail.com>
2022-04-13 12:47:25 -0600

    Update First section landing Page

--- src/components/Hero.component.js
+++ src/components/Hero.component.js
@@ -3,26 +3,45 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import ImageSlider from "./imageSlider.component";
 import { useTranslation } from "react-i18next";
+import verifyImage from '../assets/img/Check.png'
 function LightHeroE(props) {
   const [t, i18n] = useTranslation("global")
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <ImageSlider />
+    <section className="text-gray-600 body-font dark:bg-darkgray">
+      <div className="container mx-auto flex px-5 pt-10 pb-24 md:flex-row flex-col items-center md:items-start">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 flex flex-row md:flex-row-reverse justify-center">
+
+          <div class="max-w-xs bg-white rounded-xlarge border border-gray-200 shadow-md  dark:border-gray-700">
+            <div className="w-full m-5 mb-0 flex relative ">
+              <div className="w-[50px] h-[50px]  bg-circle rounded-full bg-pink-2 relative">
+                <img className="w-[25px] h-[25px] fixed bg-white rounded-full top-0 -right-3 absolute " src={verifyImage}></img>
+              </div>
+
+            </div>
+            <div className="p-6 pt-3 ">
+              <a href="#" >
+                <ImageSlider />
+              </a>
+            </div>
+            <div class="p-5">
+
+            </div>
+          </div>
         </div>
-        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center ">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            <b>Nativo NFT — V2</b> <br/>
-            {t("Landing.title")}
-          </h1>
-          <p className="mb-8 leading-relaxed">
+        <div className="lg:flex-grow md:w-1/2  flex flex-col md:items-start md:text-left items-center text-center ">
+          <div className="w-full">
+            <h2 className="dark:text-white md:text-4xl text-2xl text-center font-semibold uppercase text-left">{t("Landing.title")}</h2>
+            <div className="h-[30px]  bg-brown2 mt-[-10px] mx-auto " />
+            <h2 className="dark:text-white md:text-4xl text-2xl text-center font-semibold uppercase text-left">{t("Landing.title-2")}</h2>
+            <div className="h-[30px]  bg-brown2 mt-[-10px] mx-auto " />
+          </div>
+          <p className="m-8 leading-relaxed dark:text-white">
             {t("Landing.subtitle")}
           </p>
 
-          <div className="flex  justify-between">
+          <div className="flex  justify-between ">
             <Link to="/collections">
-              <button className="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center justify-end hover:bg-gray-200 focus:outline-none">
+              <button className="bg-white inline-flex py-3 px-5 rounded-lg items-center justify-end hover:bg-gray-200 focus:outline-none">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -38,7 +57,7 @@ function LightHeroE(props) {
                   <path d="m21.5 2h-15c-1.378 0-2.5 1.122-2.5 2.5v11c0 1.378 1.122 2.5 2.5 2.5h15c1.378 0 2.5-1.122 2.5-2.5v-11c0-1.378-1.122-2.5-2.5-2.5zm-15 2h15c.276 0 .5.224.5.5v7.099l-3.159-3.686c-.335-.393-.82-.603-1.341-.615-.518.003-1.004.233-1.336.631l-3.714 4.458-1.21-1.207c-.684-.684-1.797-.684-2.48 0l-2.76 2.759v-9.439c0-.276.224-.5.5-.5z" />
                 </svg>
                 <span className="ml-4 flex items-start flex-col leading-none">
-                  <span className="title-font font-medium">{t("Landing.btnCollections")}</span>
+                  <span className="title-font font-medium">{t("Landing.gallery")}</span>
                 </span>
               </button>
             </Link>
