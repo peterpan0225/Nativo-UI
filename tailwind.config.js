2e0e657ad8e57f782c2d9d7fd1203fc00811edc0
peterpan0225 <wasyl.0225@gmail.com>
2022-04-13 12:47:02 -0600

    Tailwind config to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    boxShadow: {
-      's': '4px 1px 17px rgba(253, 252, 252, 0.05)',
-    },
-    colors: {
-      'white': '#fff',
-      'darkgray': '#1d1d1b',
-      'yellow':'#f8de44',
-      'yellow2': '#eb8a06',
-      'brown': '#9e0904',
-      'brown2': '#d15904',
-      'yellow3':'#f6bc25',
-      'pink': '#fd644f',
-      'melon': '#f28e26',
-      'hotpink': '#ff0f47',
-      'lightpink': '#ffab96',
-      'blue': '#3c65aa',
-      'purple': '#c6379c'
-    },
     extend: {
+      boxShadow: {
+        's': '4px 1px 17px rgba(253, 252, 252, 0.05)',
+      },
+      colors: {
+        'white': '#fff',
+        'darkgray': '#1d1d1b',
+        'yellow': '#f8de44',
+        'yellow2': '#eb8a06',
+        'brown': '#9e0904',
+        'brown2': '#d15904',
+        'yellow3': '#f6bc25',
+        'pink': '#fd644f',
+        'pink-2' : "#fb5988",
+        'melon': '#f28e26',
+        'hotpink': '#ff0f47',
+        'lightpink': '#ffab96',
+        'blue': '#3c65aa',
+        'purple': '#c6379c'
+      },
+      borderRadius: {
+        'xlarge': '15px',
+      },
     }
   },
   plugins: [],
