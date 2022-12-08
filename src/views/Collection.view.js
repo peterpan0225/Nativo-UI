e395e7a5efa655cc92eacf7b798892cdc2efbb16
peterpan0225 <wasyl.0225@gmail.com>
2022-12-08 11:40:24 -0700

    fixed the title wrapped and the loading modal was changed the text and added a new spinner

--- src/views/Collection.view.js
+++ src/views/Collection.view.js
@@ -297,12 +297,18 @@ function LightHeroE(props) {
     else{
       console.log("Uploading   ")
       Swal.fire({
+        html:'<div className="text-center h-30 w-30">'
++       ' <svg role="status" class="inline mr-3 w-10 h-10 text-white  animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">'
++       ' <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>'
++       ' <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>'
++       ' </svg>'
++' </div>',
         position: 'top-center',
-        icon: 'info',
         title: t("CreateCol.uploadingAvatar"),
         showConfirmButton: false,
         
       })
+
 
       let acid = await uploadFileAPI(mint.avatar_file);
       formik.setFieldValue("avatar", acid);
@@ -477,6 +483,7 @@ function LightHeroE(props) {
             <h1 className="text-5xl font-semibold pt-10 text-center ">
               {t("MintNFT.loadMsg")}
             </h1>
+            
           </div>
         </>
       ) : (
@@ -490,6 +497,7 @@ function LightHeroE(props) {
                 className="w-full bg-white md:w-2/5 lg:w-5/12 xl:w-4/12 2xl:w-4/12 lg:items-center "
               >
                 <div name="cancel" className="mt-4 px-8   text-white  ">
+               
                   <Button
                     className=" hover:bg-black  "
                     onClick={history.goBack}
@@ -742,9 +750,10 @@ function LightHeroE(props) {
                           {/* <div className="absolute -inset-0.5 bg-gradient-to-r from-[#f2b159] to-[#ca7e16] rounded-full blur opacity-70 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt group-hover:-inset-1"></div> */}
                           <button
                             type="submit"
-                            className={`relative w-full bg-yellow2 rounded-md uppercase font-open-sans text-base px-6 py-2 font-bold border-2 border-yellow2 dark:text-white`}
+                            className={`relative w-full bg-yellow2    rounded-md uppercase font-open-sans text-base px-6 py-2 font-bold border-2 border-yellow2 dark:text-white`}
                             disabled={mint?.onSubmitDisabled}
                           >
+                             
                             {t("CreateCol.createBtn")}
                           </button>
                         </div>
@@ -786,7 +795,8 @@ function LightHeroE(props) {
                     className="w-full flex rounded-b-2xl   pt-4 px-4    bg-white h-full md:h-[100px] lg:h-[120px] xl:h-[140px]"
                   >
 
-                  <div className=" w-[120px]  h-[120px] lg:w-[140px]  lg:h-[140px] xl:w-[170px]  xl:h-[170px] -translate-y-14 xl:-translate-y-16   translate-x-2 overflow-hidden border border-white   bg-[#EBEBEB]">
+<div className="w-1/3">
+<div className=" w-[120px]  h-[120px] lg:w-[140px]  lg:h-[140px] xl:w-[170px]  xl:h-[170px] -translate-y-14 xl:-translate-y-16   translate-x-2 overflow-hidden border border-white   bg-[#EBEBEB]">
                     {mint?.avatar && (
                       <img
                         className=" w-full h-full  object-cover object-center "
@@ -795,8 +805,10 @@ function LightHeroE(props) {
                       />
                     )}
                   </div>
-                  
-                  <div className=" w-[240px]  h-[100px] -translate-y-4   translate-x-2  flex flex-col pt-2 pl-2 border border-white   bg-transparent">
+</div>
+
+  <div className="w-2/3 xl:-translate-x-4">
+ <div className=" w-full  h-[100px] -translate-y-4   translate-x-2   flex flex-col pt-2 pl-2 border border-white   bg-transparent">
                    {formik.values.title ?
                    <p className="normal-case  text-lg lg:text-xl xl:text-2xl truncate font-bold  ">
                   
@@ -814,6 +826,9 @@ function LightHeroE(props) {
                      ITEMS       
                     </p>
                   </div>
+  </div>               
+                  
+                 
 
                  
                                                
